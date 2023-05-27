ecbedd03ca6e8f59099a8900a6cccc1406536ef6
Jhon Baudin <jhonbaup0895@gmail.com>
2023-05-27 12:42:07 -0500

    fix: added response for webhook

--- routes/index.js
+++ routes/index.js
@@ -48,24 +48,15 @@ router.get("/", validateCustomHeader, (req, res) => {
  *       500:
  *         description: Internal Server Error
  */
-router.post(
-  "/webhook",
-  body("data").notEmpty().isString(),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        res.status(404).json({ errors: errors.array() });
-        return;
-      }
-      const { data } = req.body;
-      console.log(JSON.stringify(data, null, 2));
-    } catch (error) {
-      console.error("Error processing webhook:", error);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
+router.post("/webhook", async (req, res) => {
+  try {
+    console.log(JSON.stringify(req.body));
+    res.status(200).send("OK");
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-);
+});
 
 /**
  * @swagger
