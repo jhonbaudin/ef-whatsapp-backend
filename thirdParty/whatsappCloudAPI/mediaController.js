75e981f84ef16d6fb97e3974aa3912c721e04890
Jhon Baudin <jhonbaup0895@gmail.com>
2023-06-06 08:45:56 -0500

    feat: optimize mediacontroller

--- thirdParty/whatsappCloudAPI/mediaController.js
+++ thirdParty/whatsappCloudAPI/mediaController.js
@@ -11,6 +11,35 @@ ffmpeg.setFfmpegPath(ffmpegStatic);
 dotenv.config();
 
 export class MediaController {
+  mimeTypesMap = {
+    ".txt": "text/plain",
+    ".pdf": "application/pdf",
+    ".ppt": "application/vnd.ms-powerpoint",
+    ".pptx": "application/vnd.ms-powerpoint",
+    ".doc": "application/msword",
+    ".docx": "application/msword",
+    ".xls": "application/vnd.ms-excel",
+    ".xlsx": "application/vnd.ms-excel",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".mp4": "video/mp4",
+    ".3gp": "video/3gp",
+    ".webp": "image/webp",
+    ".aac": "audio/aac",
+    ".mp3": "audio/mp3",
+    ".mpeg": "audio/mpeg",
+    ".amr": "audio/amr",
+    ".ogg": "audio/ogg",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+      ".xlsx",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+      ".docx",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+      ".pptx",
+    "application/octet-stream": "",
+  };
+
   async getMedia(id) {
     const url = `https://graph.facebook.com/${process.env.WP_API_VERSION}/${id}`;
     try {
@@ -31,16 +60,6 @@ export class MediaController {
     }
   }
 
-  async convertAudioToMP3(inputFilePath, outputFilePath) {
-    return new Promise((resolve, reject) => {
-      ffmpeg(inputFilePath)
-        .output(outputFilePath)
-        .on("end", () => resolve())
-        .on("error", (error) => reject(error))
-        .run();
-    });
-  }
-
   async uploadMedia(base64File, mime_type) {
     const fileExtension = this.getExtensionFromMimeType(mime_type);
     const tempFilename = crypto.randomBytes(4).toString("hex");
@@ -48,14 +67,18 @@ export class MediaController {
     let convertedFilePath = filePath;
 
     try {
-      await fs.writeFileSync(filePath, base64File.replace(/^data:[^,]+,/, ""), {
-        encoding: "base64",
-      });
+      await fs.promises.writeFile(
+        filePath,
+        base64File.replace(/^data:[^,]+,/, ""),
+        {
+          encoding: "base64",
+        }
+      );
 
       const url = `https://graph.facebook.com/${process.env.WP_API_VERSION}/${process.env.WP_PHONE_ID}/media`;
       const formData = new FormData();
 
-      if (mime_type == "audio/mpeg") {
+      if (mime_type === "audio/mpeg") {
         convertedFilePath = path.join("./tmp/", `${tempFilename}.mp3`);
         await this.convertAudioToMP3(filePath, convertedFilePath);
       }
@@ -63,9 +86,7 @@ export class MediaController {
       formData.append("file", fs.createReadStream(convertedFilePath), {
         contentType: mime_type,
       });
-
       formData.append("messaging_product", "whatsapp");
-
       const response = await fetch(url, {
         method: "POST",
         body: formData,
@@ -131,53 +152,6 @@ export class MediaController {
     }
   }
 
-  getMimeType(fileExtension) {
-    switch (fileExtension) {
-      case ".txt":
-        return "text/plain";
-      case ".pdf":
-        return "application/pdf";
-      case ".ppt":
-      case ".pptx":
-        return "application/vnd.ms-powerpoint";
-      case ".doc":
-      case ".docx":
-        return "application/msword";
-      case ".xls":
-      case ".xlsx":
-        return "application/vnd.ms-excel";
-      case ".jpg":
-      case ".jpeg":
-        return "image/jpeg";
-      case ".png":
-        return "image/png";
-      case ".mp4":
-        return "video/mp4";
-      case ".3gp":
-        return "video/3gp";
-      case ".webp":
-        return "image/webp";
-      case ".aac":
-        return "audio/aac";
-      case ".mp3":
-        return "audio/mp3";
-      case ".mpeg":
-        return "audio/mpeg";
-      case ".amr":
-        return "audio/amr";
-      case ".ogg":
-        return "audio/ogg";
-      case ".xlsx":
-        return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-      case ".docx":
-        return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-      case ".pptx":
-        return "application/vnd.openxmlformats-officedocument.presentationml.presentation";
-      default:
-        return "application/octet-stream";
-    }
-  }
-
   getFileExtension(headers) {
     const contentType = headers.get("content-type");
     if (contentType) {
@@ -190,31 +164,22 @@ export class MediaController {
   }
 
   getExtensionFromMimeType(mimeType) {
-    const mimeTypesMap = {
-      "text/plain": ".txt",
-      "application/pdf": ".pdf",
-      "application/vnd.ms-powerpoint": ".ppt",
-      "application/msword": ".doc",
-      "application/vnd.ms-excel": ".xls",
-      "image/jpeg": ".jpg",
-      "image/png": ".png",
-      "video/mp4": ".mp4",
-      "video/3gp": ".3gp",
-      "image/webp": ".webp",
-      "audio/aac": ".aac",
-      "audio/mp3": ".mp3",
-      "audio/mpeg": ".mpeg",
-      "audio/amr": ".amr",
-      "audio/ogg": ".ogg",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        ".xlsx",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        ".docx",
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation":
-        ".pptx",
-      "application/octet-stream": "",
-    };
+    return Object.keys(this.mimeTypesMap).find(
+      (extension) => this.mimeTypesMap[extension] === mimeType
+    );
+  }
 
-    return mimeTypesMap[mimeType] || "";
+  getMimeType(fileExtension) {
+    return this.mimeTypesMap[fileExtension] || "application/octet-stream";
+  }
+
+  async convertAudioToMP3(inputFilePath, outputFilePath) {
+    return new Promise((resolve, reject) => {
+      ffmpeg(inputFilePath)
+        .output(outputFilePath)
+        .on("end", resolve)
+        .on("error", reject)
+        .run();
+    });
   }
 }
