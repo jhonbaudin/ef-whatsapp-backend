c4b6630b222739efd7a7b9fdf2841c433cc90605
Jhon Baudin <jhonbaup0895@gmail.com>
2023-07-21 00:05:44 -0500

    refactor: restart pm2 bny cron

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,14 +5,14 @@ module.exports = {
       script: "./server.js",
       instances: 1,
       max_memory_restart: "2G",
-      exp_backoff_restart_delay: 100,
+      cron_restart: "*/5 * * * * *",
     },
     {
       name: "ef-whatsapp-dev",
       script: "./server.js",
       instances: 1,
       max_memory_restart: "200M",
-      exp_backoff_restart_delay: 100,
+      cron_restart: "*/20 * * * * *",
     },
   ],
 };
