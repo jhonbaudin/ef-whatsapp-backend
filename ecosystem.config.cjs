1995f18e8f310a96b5fb01db9c341af01f20f136
Jhon Baudin <jhonbaup0895@gmail.com>
2023-07-20 23:55:38 -0500

    refactor: added instances

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,27 +4,14 @@ module.exports = {
       name: "ef-whatsapp-prod",
       script: "./server.js",
       instances: 1,
-      autorestart: true,
-      watch: false,
-
       max_memory_restart: "2G",
-      env: {
-        NODE_ENV: "production",
-      },
-      restartOnError: true,
       exp_backoff_restart_delay: 100,
     },
     {
       name: "ef-whatsapp-dev",
       script: "./server.js",
       instances: 1,
-      autorestart: true,
-      watch: true,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "dev",
-      },
-      restartOnError: true,
+      max_memory_restart: "200M",
       exp_backoff_restart_delay: 100,
     },
   ],
