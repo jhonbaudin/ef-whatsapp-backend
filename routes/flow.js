7a89bf3b50b562fe78a4a5f0aa8e90e6844581c1
Jhon Baudin <jhonbaup0895@gmail.com>
2023-08-13 17:58:08 -0500

    refactor: changed body per path params in flow

--- routes/flow.js
+++ routes/flow.js
@@ -17,19 +17,17 @@ export default function flowRoutes(pool) {
 
   /**
    * @swagger
-   * /flow:
+   * /flow/{company_phone_id}:
    *   get:
    *     summary: Get all flows
    *     tags: [Flow]
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type: object
-   *             properties:
-   *               company_phone_id:
-   *                 type: string
+   *     parameters:
+   *       - in: path
+   *         name: company_phone_id
+   *         schema:
+   *           type: integer
+   *         required: true
+   *         description: Company phone ID
    *     responses:
    *       200:
    *         description: Returns a list of flows
@@ -38,23 +36,44 @@ export default function flowRoutes(pool) {
    *       500:
    *         description: Failed to get flows
    */
-  router.get("/", verifyToken, validateCustomHeader, async (req, res) => {
-    const { user, company_phone_id } = req.body;
+  router.get(
+    "/:company_phone_id",
+    verifyToken,
+    validateCustomHeader,
+    async (req, res) => {
+      const { user } = req.body;
+      const { company_phone_id } = req.params;
 
-    try {
-      const flows = await flowModel.getFlows(user.company_id, company_phone_id);
-      res.json(flows);
-    } catch (error) {
-      res.status(500).json({ message: "Error getting the flows." });
+      if (!company_phone_id) {
+        res.status(400).json({ message: "Required parameters are missing." });
+        return;
+      }
+
+      try {
+        const flows = await flowModel.getFlows(
+          user.company_id,
+          company_phone_id
+        );
+        res.json(flows);
+      } catch (error) {
+        res.status(500).json({ message: "Error getting the flows." });
+      }
     }
-  });
+  );
 
   /**
    * @swagger
-   * /flow:
+   * /flow/{company_phone_id}:
    *   post:
    *     summary: Create or update flows
    *     tags: [Flow]
+   *     parameters:
+   *       - in: path
+   *         name: company_phone_id
+   *         schema:
+   *           type: integer
+   *         required: true
+   *         description: Company phone ID
    *     requestBody:
    *       required: true
    *       content:
@@ -87,24 +106,34 @@ export default function flowRoutes(pool) {
    *       500:
    *         description: Failed to create or update flows
    */
-  router.post("/", verifyToken, validateCustomHeader, async (req, res) => {
-    const { flow, user, company_phone_id } = req.body;
+  router.post(
+    "/:company_phone_id",
+    verifyToken,
+    validateCustomHeader,
+    async (req, res) => {
+      const { flow, user } = req.body;
+      const { company_phone_id } = req.params;
 
-    if (!flow) {
-      res.status(400).json({ message: "Required parameters are missing." });
-      return;
-    }
+      if (!flow || !company_phone_id) {
+        res.status(400).json({ message: "Required parameters are missing." });
+        return;
+      }
 
-    try {
-      await flowModel.createUpdateFlow(flow, user.company_id, company_phone_id);
-      res.json({ message: "Flows created or updated successfully." });
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: "Error creating or updating the flows." });
+      try {
+        await flowModel.createUpdateFlow(
+          flow,
+          user.company_id,
+          company_phone_id
+        );
+        res.json({ message: "Flows created or updated successfully." });
+      } catch (error) {
+        console.log(error);
+        res
+          .status(500)
+          .json({ message: "Error creating or updating the flows." });
+      }
     }
-  });
+  );
 
   return router;
 }
