a927efc7c9f18a45725a18eaae6825fc9ed9390f
Jhon Baudin <jhonbaup0895@gmail.com>
2023-12-02 00:00:30 -0500

    fix: validate limit rules

--- models/ConversationModel.js
+++ models/ConversationModel.js
@@ -121,7 +121,7 @@ export class ConversationModel {
     let totalPages = 1;
     let currentPage = 1;
 
-    if (!initDate && !endDate && limitF == "") {
+    if ((!initDate || !endDate) && limitF == "") {
       throw new Error("Incorrect parameters");
     }
 
