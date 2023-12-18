4b981010ffc29a5be91b8edfcb4d865ffbade4cc
Jhon Baudin <jhonbaup0895@gmail.com>
2023-12-18 01:25:58 -0500

    feat: donwload reports

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -46,11 +46,11 @@ export class ReportModel {
         JOIN
             companies_phones cp ON c.company_phone_id = cp.id
         WHERE
-            m.status = 'client'
+            m.status = $3
             AND TO_TIMESTAMP(m.created_at) BETWEEN $1 AND $2
         GROUP BY
             c.company_phone_id, cp.phone, cp.alias`,
-        [initDate, endDate]
+        [initDate, endDate, "client"]
       );
 
       const campaingMessages = await client.query(
@@ -92,6 +92,156 @@ export class ReportModel {
       report.receivedMessages = receivedMessages.rows;
       report.campaingMessages = campaingMessages.rows;
       report.totalConversations = totalConversations.rows;
+
+      return report;
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("Error fetching report");
+    } finally {
+      await client.release(true);
+    }
+  }
+
+  async getDetailsReport(company_id, initDate = null, endDate = null) {
+    const client = await this.pool.connect();
+    if ((!initDate || !endDate) && limit == "") {
+      throw new Error("Incorrect parameters");
+    }
+    try {
+      const report = {
+        conversaciones: [],
+        mensajesRecibidos: [],
+        mensajesEnviados: [],
+        mensajesEnviadosBot: [],
+        mensajesRecibidosCampanias: [],
+      };
+      const conversaciones = await client.query(
+        `SELECT
+            c.id,
+            cp.alias,
+            cp.phone AS tlf_empresa,
+            to_timestamp(c.last_message_time) AS fecha_ultimo_mensaje,
+            c2.phone AS tlf_cliente,
+            c2."name" AS nombre_cliente
+        FROM
+            conversations c
+        JOIN
+            companies_phones cp ON cp.id = c.company_phone_id
+        JOIN
+            contacts c2 ON c.contact_id = c2.id 
+        WHERE
+            c.created_at BETWEEN $1 AND $2;
+        `,
+        [initDate, endDate]
+      );
+
+      const mensajesRecibidos = await client.query(
+        `SELECT
+            m.id,
+            message_type AS tipo_mensaje,
+            c.id AS id_conversacion,
+            cp.alias,
+            cp.phone AS tlf_empresa,
+            c2.phone AS tlf_cliente,
+            c2."name" AS nombre_cliente,
+            to_timestamp(m.created_at) AS fecha_mensaje
+        FROM
+            messages m
+        JOIN
+            conversations c ON c.id = m.conversation_id
+        JOIN
+            companies_phones cp ON cp.id = c.company_phone_id
+        JOIN
+            contacts c2 ON c.contact_id = c2.id
+        WHERE
+            to_timestamp(m.created_at) BETWEEN $1 AND $2
+            AND m.status = $3
+        `,
+        [initDate, endDate, "client"]
+      );
+
+      const mensajesEnviados = await client.query(
+        `SELECT
+            m.id,
+            message_type AS tipo_mensaje,
+            c.id AS id_conversacion,
+            cp.alias,
+            cp.phone AS tlf_empresa,
+            c2.phone AS tlf_cliente,
+            c2."name" AS nombre_cliente,
+            to_timestamp(m.created_at) AS fecha_mensaje
+        FROM
+            messages m
+        JOIN
+            conversations c ON c.id = m.conversation_id
+        JOIN
+            companies_phones cp ON cp.id = c.company_phone_id
+        JOIN
+            contacts c2 ON c.contact_id = c2.id
+        WHERE
+            to_timestamp(m.created_at) BETWEEN $1 AND $2
+            AND m.status <> $3;
+        `,
+        [initDate, endDate, "client"]
+      );
+
+      const mensajesEnviadosBot = await client.query(
+        `SELECT
+            q.id,
+            q.message,
+            c.id AS id_conversacion,
+            cp.alias,
+            cp.phone AS tlf_empresa,
+            c2.phone AS tlf_cliente,
+            c2."name" AS nombre_cliente,
+            q.created_at AS fecha_mensaje
+        FROM
+            queue q
+        JOIN
+            conversations c ON c.id = q.conversation_id
+        JOIN
+            companies_phones cp ON cp.id = c.company_phone_id
+        JOIN
+            contacts c2 ON c.contact_id = c2.id
+        WHERE
+            q.created_at BETWEEN $1 AND $2;
+        `,
+        [initDate, endDate]
+      );
+
+      const mensajesRecibidosCampanias = await client.query(
+        `SELECT
+            m.id,
+            message_type AS tipo_mensaje,
+            c.id AS id_conversacion,
+            cp.alias,
+            cp.phone AS tlf_empresa,
+            c2.phone AS tlf_cliente,
+            c2."name" AS nombre_cliente,
+            to_timestamp(m.created_at) AS fecha_mensaje
+        FROM
+            messages m
+        JOIN
+            messages_referral mr ON	mr.message_id = m.id
+        JOIN
+            conversations c ON c.id = m.conversation_id
+        JOIN
+            companies_phones cp ON cp.id = c.company_phone_id
+        JOIN
+            contacts c2 ON c.contact_id = c2.id
+        WHERE
+            to_timestamp(m.created_at) BETWEEN $1 AND $2
+            AND m.status = $3
+        `,
+        [initDate, endDate, "client"]
+      );
+
+      report.conversaciones = conversaciones.rows;
+      report.mensajesRecibidos = mensajesRecibidos.rows;
+      report.mensajesEnviados = mensajesEnviados.rows;
+      report.mensajesEnviadosBot = mensajesEnviadosBot.rows;
+      report.mensajesRecibidosCampanias = mensajesRecibidosCampanias.rows;
 
       return report;
     } catch (error) {
--- routes/reports.js
+++ routes/reports.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.js";
 import { validateCustomHeader } from "../middlewares/customHeader.js";
 import { ReportModel } from "../models/ReportModel.js";
+import ExcelJS from "exceljs";
 
 const router = Router();
 
@@ -55,6 +56,119 @@ export default function reportRoutes(pool) {
       res.status(500).json({ message: "Error getting the report." });
     }
   });
+
+  /**
+   * @swagger
+   * /report/donwload:
+   *   get:
+   *     tags: [Report]
+   *     summary: Donwload reports
+   *     parameters:
+   *       - in: query
+   *         name: initDate
+   *         schema:
+   *           type: date
+   *         description: Filter by init date, yyyy-m-d H:i:s
+   *       - in: query
+   *         name: endDate
+   *         schema:
+   *           type: date
+   *         description: Filter by end date, yyyy-m-d H:i:s
+   *     responses:
+   *       200:
+   *         description: Returns the XLS report
+   *       401:
+   *         description: Unauthorized access
+   *       500:
+   *         description: Error downloading the report
+   */
+  router.get(
+    "/donwload",
+    verifyToken,
+    validateCustomHeader,
+    async (req, res) => {
+      const { user } = req.body;
+      let { initDate, endDate } = req.query;
+
+      try {
+        const report = await reportModel.getDetailsReport(
+          user.company_id,
+          initDate,
+          endDate
+        );
+
+        const workbook = new ExcelJS.Workbook();
+
+        const sheet1 = workbook.addWorksheet("conversaciones");
+        const sheet2 = workbook.addWorksheet("mensajesRecibidos");
+        const sheet3 = workbook.addWorksheet("mensajesEnviados");
+        const sheet4 = workbook.addWorksheet("mensajesEnviadosBot");
+        const sheet5 = workbook.addWorksheet("mensajesRecibidosCampanias");
+
+        const headers1 = report.conversaciones.length
+          ? Object.keys(report.conversaciones[0])
+          : [];
+        sheet1.addRow(headers1);
+        report.conversaciones.forEach((fila) => {
+          sheet1.addRow(Object.values(fila));
+        });
+
+        const headers2 = report.mensajesRecibidos.length
+          ? Object.keys(report.mensajesRecibidos[0])
+          : [];
+        sheet2.addRow(headers2);
+        report.mensajesRecibidos.forEach((fila) => {
+          sheet2.addRow(Object.values(fila));
+        });
+
+        const headers3 = report.mensajesEnviados.length
+          ? Object.keys(report.mensajesEnviados[0])
+          : [];
+        sheet3.addRow(headers3);
+        report.mensajesEnviados.forEach((fila) => {
+          sheet3.addRow(Object.values(fila));
+        });
+
+        const headers4 = report.mensajesEnviadosBot.length
+          ? Object.keys(report.mensajesEnviadosBot[0])
+          : [];
+        sheet4.addRow(headers4);
+        report.mensajesEnviadosBot.forEach((fila) => {
+          sheet4.addRow(Object.values(fila));
+        });
+
+        const headers5 = report.mensajesRecibidosCampanias.length
+          ? Object.keys(report.mensajesRecibidosCampanias[0])
+          : [];
+        sheet5.addRow(headers5);
+        report.mensajesRecibidosCampanias.forEach((fila) => {
+          sheet5.addRow(Object.values(fila));
+        });
+
+        res.setHeader(
+          "Content-Type",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+        res.setHeader(
+          "Content-Disposition",
+          "attachment; filename=reporte.xlsx"
+        );
+
+        return workbook.xlsx
+          .write(res)
+          .then(() => {
+            res.status(200).end();
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).json({ message: "Error downloading the report." });
+          });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error downloading the report." });
+      }
+    }
+  );
 
   return router;
 }
